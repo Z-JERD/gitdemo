83dc0499567f90e90ec5217ecd6da105c835a133
zhaoguangfei <zhaoguangfei@example.com>
2021-05-20 16:54:42 +0800

    紧急提交

--- m1_demo.py
+++ m1_demo.py
@@ -25,4 +25,19 @@ git commit 20210514144700
 git commit 20210514150000 m1_demo
 
 checkout mas20210517
+
+def url__chain_regulate__seatmap_list(self, cinema_code: int, status: int = None) -> list:
+       
+     获取座位图模板
+     :param  cinema_code
+     :param status: 1： 模板未使用 2：模板已使用
+     :return:
+       
+      assert status in [None, 1, 2], '类型值有误'
+
+      chain_user, cinema_user = self.chain_cinema_user(cinema_code)
+
+      response = self.ckt.get_seatmap_list(cinema_code, status)
+
+       return response
 """
