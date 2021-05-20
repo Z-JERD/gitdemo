54c20eee1c67c1df31a4e43302ae4d6bfb9078c4
zhaoguangfei <zhaoguangfei@example.com>
2021-05-20 16:51:06 +0800

    暂停修改

--- m1_demo.py
+++ m1_demo.py
@@ -25,4 +25,12 @@ git commit 20210514144700
 git commit 20210514150000 m1_demo
 
 checkout mas20210517
+
+def url__chain_regulate__save_seatmap(self, cinema_code: int, map_info: list) -> bool:
+
+    chain_user, cinema_user = self.chain_cinema_user(cinema_code)
+
+    self.ckt.save_seatmap_template(cinema_code, map_info)
+
+    return True
 """
