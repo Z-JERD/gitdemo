6083420ed21958b17a029991843f8e060196745e
Jockfi <40668185+Z-JERD@users.noreply.github.com>
2021-06-02 19:40:23 +0800

    post model

--- m2_demo.py
+++ m2_demo.py
@@ -4,5 +4,7 @@ Connecting to 10.110.30.144:22 To escape to local shell, press
 
 git test on github in 202106021929
 
+POST /cinema_regulate/get_server_models
+
 GET /cinema_regulate/get_manufactures_models
 
