b2f8018807e6ca8a71df8b414d04b68cb7828db6
Jockfi <40668185+Z-JERD@users.noreply.github.com>
2021-06-02 19:47:47 +0800

    subprocess

--- m2_demo.py
+++ m2_demo.py
@@ -1,9 +1,9 @@
 
 Connecting to 10.110.30.144:22 To escape to local shell, press
 
-git test on github in 202106021929
+git test on github in 202106021947
 
-POST /cinema_regulate/get_server_models
+subprocess 3287775 exited with code 0
 
 GET /cinema_regulate/get_manufactures_models
 
