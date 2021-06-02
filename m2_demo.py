e0deff0280f0112c799e57cebc88b32df5ae2546
zhaoguangfei <zhaoguangfei@example.com>
2021-06-02 19:41:40 +0800

    Last login

--- m2_demo.py
+++ m2_demo.py
@@ -1,8 +1,8 @@
 
-
 Connecting to 10.110.30.144:22 To escape to local shell, press
 
 git test on github in 202106021929
 
 GET /cinema_regulate/get_manufactures_models
 
+Last login: Wed Jun  2 18:46:06 2021 from 10.110.30.114
