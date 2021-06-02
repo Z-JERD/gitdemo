9927cb2e051b92bbdbf33307549dfe9a1fc33e0d
Jockfi <40668185+Z-JERD@users.noreply.github.com>
2021-06-02 18:49:00 +0800

    Update m_demo.py

--- m_demo.py
+++ m_demo.py
@@ -17,7 +17,7 @@ new a tag v1
 git commit --amend
 
 
-git reset --hard 460cd86 in 20210514
+git reset --hard 460cd86 in 20210202
 merge:
 git commit 202105141500 m_demo
 
@@ -25,7 +25,7 @@ update desk
 
  assert group_code in [0, 1, 2], '坐标轴（{}，{}）座位状态有误'.format(x_coord, y_coord)
             assert status in [1, 2, 3], '坐标轴（{}，{}）座位属性有误'.format(x_coord, y_coord)
-
-update in 20210524
+merge demo
+update in 20210602
 """
 
