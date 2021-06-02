ed42d46e7d2226e35fe0d379c75c79022c591781
zhaoguangfei <zhaoguangfei@example.com>
2021-06-02 19:57:00 +0800

    195501

--- m2_demo.py
+++ m2_demo.py
@@ -1,9 +1,9 @@
 
 Connecting to 10.110.30.144:22 To escape to local shell, press
 
-git test on github in 202106021947
+git test on github in 20210602195501
 
-subprocess 3287775 exited with code 0
+detected binary path: /usr/local/uapstack/msexec/bin/uwsgi
 
 GET /cinema_regulate/get_manufactures_models
 
